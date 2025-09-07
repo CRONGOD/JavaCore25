923717e9605b6fcf5cda36f201725535d41e2c26
KarenCallix <karen@callix.com>
2025-09-07 12:27:01 -0700

    homework 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
  class Example {
      public static void main (String[] args) {
          for (int i = 1; i <= 10; i++) {
-             if (i % 2 == 0) {}
+           if (i % 2 == 0) {}
          }
          System.out.println("hello world!");
+         int x = 1;
+         int y = 2;
+         if (x == y) {}
      }
  }
