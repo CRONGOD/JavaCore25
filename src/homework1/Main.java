d8a7244ed7c3b26bd0f9a99260eefcd7e9884c42
KarenCallix <karen@callix.com>
2025-09-09 12:43:19 -0700

    homework1

---
+++ src/homework1/Main.java
@@ -0,0 +1,25 @@
+package homework1 ;
+public class Main {
+    public static void main(String[] args) {
+    int x = 10;
+    int y = 20;
+    if (x < y){
+        System.out.println("x больше y");
+    }
+    for ( int i = 1 ; i <= 5 ; i++ ){
+        System.out.println("i = " + i );
+
+
+    }
+    int a = 5;
+    int b = 7;
+    int c = a + b;
+    System.out.println("a + b" + c);
+
+    int n = 3;
+    for (int i = 1; i <= 10 ; i++ ){
+        System.out.println(n +" x " + i + " = " + (n * i) );
+    }
+    }
+}
+
